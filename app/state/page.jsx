6375fbc6dadec1ee7bebd652f8edaf6cea37e918
Vishal kumar <vishalk80269@gmail.com>
2023-12-30 16:16:46 +0530

    event and state in next js

---
+++ app/state/page.jsx
@@ -0,0 +1,15 @@
+"use client";
+import { useState } from "react";
+
+const page = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+    </>
+  );
+};
+
+export default page;
