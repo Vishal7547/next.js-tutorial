7e697775ed7c0a7f73bbda5ee86714810c23ee23
Vishal kumar <vishalk80269@gmail.com>
2024-01-11 02:49:00 +0530

    complete frontend

--- app/component/Product.jsx
+++ app/component/Product.jsx
@@ -4,7 +4,7 @@ const Product = ({ title, price }) => {
   return (
     <div style={{ border: "1px solid black", margin: "10px", padding: "10px" }}>
       <h4>Title: {title}</h4>
-      <h4>price: {price}</h4>
+      <h4>Price: {price}</h4>
     </div>
   );
 };
